5b56df3e425ab2ed06a3bef46f730bb8ed804a61
Moussa Diatta <diattamoussa010@gmail.com>
2025-10-13 09:50:56 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-blue-500 p-4 text-white text-xl font-bold">
+      Tailwind CSS fonctionne ! 🎉
+    </div>
+    </>
+  )
+}
+
+export default App
